4cb763a5abc1b7924e7bf9909bf7ff7982ed6dbc
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 17:10:09 +0300

    Create BoolDemo.java

---
+++ 02-data-types-and-operators/BoolDemo.java
@@ -0,0 +1,15 @@
+public class BoolDemo {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("b is " + b);
+
+        b = true;
+        System.out.println("b is " + b);
+
+        // a boolean value can control the if statement
+        if (b) System.out.println("This is not executed.")
+        System.out.println("10 > 9 is " + (10 > 9));
+    }
+}
