ff42c450b038868042ef578a9a6bb20384575dbc
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 17:14:14 +0300

    Create Sound.java

---
+++ 02-data-types-and-operators/Sound.java
@@ -0,0 +1,12 @@
+public class Sound {
+
+	public static void main(String[] args) {
+		double distance;
+		
+		distance = 7.2 * 1.100;
+		
+		System.out.println("The lightning is " + distance + " feet away.");
+
+	}
+
+}
