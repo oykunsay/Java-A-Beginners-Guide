1b52fc450a1f77bfbe8f2a30b3faf9a0b8e2bc99
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 17:00:47 +0300

    Create CharArithDemo.java

---
+++ 02-data-types-and-operators/CharArithDemo.java
@@ -0,0 +1,14 @@
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch is now " + ch);
+
+        ch++;
+        System.out.println("ch is now " + ch);
+
+        ch = 90;
+        System.out.println("ch is now " + ch);
+    }
+}
