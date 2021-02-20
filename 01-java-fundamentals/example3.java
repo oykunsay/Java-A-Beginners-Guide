a632827100eaeb48b54fe881d679c24526693b88
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 14:08:52 +0300

    Update example3.java

--- 01-java-fundamentals/example3.java
+++ 01-java-fundamentals/example3.java
@@ -7,12 +7,12 @@ public class Example3 {
 		v=10;
 		x=10.0;
 		
-		System.out.println("Original value of v:" + v);
-		System.out.println("Original value of x:" + x);
+		System.out.println("Original value of v:" + v); // 10
+		System.out.println("Original value of x:" + x); // 10.0
 		System.out.println();
 		
 		v = v/4;
 		x=x/4;
 		
-		System.out.println("v after division:"+ v);
-		System.out.println("x after division:" + x);
+		System.out.println("v after division:"+ v); // 2.0
+		System.out.println("x after division:" + x); // 2.5
