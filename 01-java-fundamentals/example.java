94e60109c22c308fd598810cfbf5f550c74570a5
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 13:33:41 +0300

    Update example.java

--- 01-java-fundamentals/example.java
+++ 01-java-fundamentals/example.java
@@ -1,5 +1,8 @@
+/* This is a simple Java Program.
+   Call this file Example.java.
+*/
 public class Example {
-
+	// A Java program begins with a call to main().
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
