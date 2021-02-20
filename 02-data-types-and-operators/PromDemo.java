584abc5f2b8d4d6fd651e5900906a96a98686c82
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 18:24:00 +0300

    Create PromDemo.java

---
+++ 02-data-types-and-operators/PromDemo.java
@@ -0,0 +1,9 @@
+public class PromDemo {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        int i = b * b;
+        b = (byte) (b * b);
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
