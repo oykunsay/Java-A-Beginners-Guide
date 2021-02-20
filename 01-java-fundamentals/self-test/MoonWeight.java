df5ffdca521676c1175c0115f1e74417d5c52a55
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 16:30:04 +0300

    Create MoonWeight.java

---
+++ 01-java-fundamentals/self-test/MoonWeight.java
@@ -0,0 +1,13 @@
+public class MoonWeight {
+
+	public static void main(String[] args) {
+		double earthweight, moonweight;
+		
+		earthweight = 83.0;
+		
+		moonweight = earthweight * 0.17;
+				
+		System.out.println("Your moonweight is " + moonweight); // My moon weight is 14.110000000000001
+
+	}
+}
