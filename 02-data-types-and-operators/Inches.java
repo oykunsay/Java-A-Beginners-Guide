e68ed19ade7b5b113eb19bad0410a565e5f48f9d
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-20 16:55:02 +0300

    Create Inches.java

---
+++ 02-data-types-and-operators/Inches.java
@@ -0,0 +1,11 @@
+public class Inches {
+    public static void main(String[] args) {
+        long ci, im;
+
+        im = 5280 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("There are " + ci + " cubic inches in cubic mile.");
+    }
+}
