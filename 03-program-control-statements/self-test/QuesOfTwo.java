181679f3675716eaa18df69ef3b1bca76d17d786
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-21 15:17:45 +0300

    Create QuesOfTwo.java

---
+++ 03-program-control-statements/self-test/QuesOfTwo.java
@@ -0,0 +1,9 @@
+public class QuesOfTwo {
+
+    public static void main(String[] args) {
+        int max_two = 1024;
+        for (int i = 1; i <= max_two; i *= 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
