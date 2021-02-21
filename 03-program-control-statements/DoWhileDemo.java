18083c268c6a61ffce062b6790d0176805651876
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-21 12:31:14 +0300

    Create DoWhileDemo.java

---
+++ 03-program-control-statements/DoWhileDemo.java
@@ -0,0 +1,12 @@
+public class DoWhileDemo {
+    public static void main(String args[])
+            throws java.io.IOException {
+
+        char ch;
+
+        do {
+            System.out.print("Press a key following by ENTER: ");
+            ch = (char) System.in.read(); // get a char
+        } while(ch != 'q');
+    }
+}
