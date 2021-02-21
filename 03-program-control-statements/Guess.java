b587fed10b996cf7a5d42c84320125c8a233b79d
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-21 11:02:52 +0300

    Update Guess.java

--- 03-program-control-statements/Guess.java
+++ 03-program-control-statements/Guess.java
@@ -1,15 +1,23 @@
 public class Guess {
-
-    public static void main(String[] args) throws java.io.IOException {
+    public static void main(String[] args)
+        throws java.io.IOException {
         char ch, answer = 'K';
-        System.out.println("Guess the character: ");
+
+        System.out.println("I'm thinking of a letter between A and Z.");
+        System.out.print("Can u guess it: ");
+
         ch = (char) System.in.read();
+
         if (ch == answer) {
-            System.out.println("Yep");
-        } else if (ch < answer) {
-            System.out.println("Nope, too low");
+            System.out.println("**Right**");
         } else {
-            System.out.println("Nope, too high");
+            System.out.println("...Sorry, you're wrong.");
+            // a nested if
+            if (ch < answer) {
+                System.out.println("Too low");
+            } else {
+                System.out.println("Too high");
+            }
         }
     }
 }
