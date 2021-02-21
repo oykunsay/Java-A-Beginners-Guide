a3bcbe207ab693ae8eaaeb077b4fbee5a9528612
oykunsay <67629217+oykunsay@users.noreply.github.com>
2021-02-21 12:28:29 +0300

    Create WhileDemo.java

---
+++ 03-program-control-statements/WhileDemo.java
@@ -0,0 +1,10 @@
+public class WhileDemo {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        while (ch <= 'z') {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
